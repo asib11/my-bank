f5520011d9802107ebd562430aed585c197c5da7
asib <ahmedasib1@gmail.com>
2023-08-18 23:57:30 +0600

    some modification and total balance calculation

--- js/inside.js
+++ js/inside.js
@@ -2,22 +2,46 @@
 document.getElementById("deposit-btn").addEventListener('click',()=>{
 
     const depositField = document.getElementById("deposit-input");
+    const depositFieldValue = depositField.value;
+    depositField.value = '';
+    if(isNaN(depositFieldValue)){
+        alert('please provide a vaild number');
+        return;
+    }
     const deposit = document.getElementById("deposit");
     const previousDeposit = deposit.innerText;
-    const depositTotal = Number(previousDeposit) + Number(depositField.value);
+    const depositTotal = Number(previousDeposit) + Number(depositFieldValue);
     deposit.innerText = depositTotal;
-    depositField.value = '';
+
+    const total = document.getElementById("total");
+    const totalBalance = total.innerText;
+    const newTotalBalance = Number(totalBalance)+ Number(depositFieldValue);
+    total.innerText = newTotalBalance;
 
 });
 // withdraw
 document.getElementById("withdraw-btn").addEventListener('click',()=>{
 
     const withdrawField = document.getElementById("withdraw-input");
+    const withdrawFieldValue = withdrawField.value;
     const withdraw = document.getElementById("withdraw");
     const previousWithdraw = withdraw.innerText;
-    const withdrawTotal = Number(previousWithdraw) + Number(withdrawField.value);
-    withdraw.innerText = withdrawTotal;
     withdrawField.value = '';
+    if(isNaN(withdrawFieldValue)){
+        alert('please provide a vaild number');
+        return;
+    }
+    
+    const total = document.getElementById("total");
+    const totalBalance = total.innerText;
+    if(totalBalance >= withdrawFieldValue){
+        const withdrawTotal = Number(previousWithdraw) + Number(withdrawFieldValue);
+        withdraw.innerText = withdrawTotal;
+        const newTotalBalance = Number(totalBalance)- Number(withdrawFieldValue);
+        total.innerText = newTotalBalance;
+    }else{
+        alert('insuffience balance');
+    }
+    
 });
 
-const total = document.getElementById("total");
