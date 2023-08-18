adc3e4c829d91114ae8654d4ba6578e707f930f8
asib <ahmedasib1@gmail.com>
2023-08-18 16:17:41 +0600

    withdraw section complete

--- js/inside.js
+++ js/inside.js
@@ -9,3 +9,17 @@ document.getElementById("deposit-btn").addEventListener('click',()=>{
     depositField.value = '';
 
 });
+// withdraw
+document.getElementById("withdraw-btn").addEventListener('click',()=>{
+
+    const withdrawField = document.getElementById("withdraw-input");
+    const withdraw = document.getElementById("withdraw");
+    const previousWithdraw = withdraw.innerText;
+    const withdrawTotal = Number(previousWithdraw) - Number(withdrawField.value);
+    if(withdrawTotal>= 0){
+        withdraw.innerText = withdrawTotal;
+    }else{
+        alert("insufficient balance")
+    }
+    withdrawField = '';
+});
